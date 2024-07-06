93fe33e3dc2c3d4e595aa51b7fb86440465f36ec
oleksiiDranenko <alexdranenko2004@icloud.com>
2024-07-06 22:40:18 +0100

    local storage fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -79,6 +79,7 @@ export default function Navbar() {
 
             } else {
                 dispatch(logOut())
+                localStorage.removeItem('userId')
             }
 
             setLoading(false)
